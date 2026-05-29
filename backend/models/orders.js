5094c7989a3d3208b949c0ef2fe84a5992a2ce7a
Sai Venkata Siva Raju Pottabathula <spottabathula@phizzle.com>
2026-05-29 10:17:37 +0530

    orders model

---
+++ backend/models/orders.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    products: [{
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        quantity: { type: Number, required: true },
+    }],
+    totalPrice: { type: Number, required: true },
+    status: { type: String, enum: ['pending', 'shipped', 'delivered', 'cancelled'], default: 'pending' },
+    address: { type: String, required: true },
+    paymentMethod: { type: String, required: true },
+    paymentid: { type: String },
+    paymentStatus: { type: String, enum: ['pending', 'completed', 'failed'], default: 'pending' },
+}, { timestamps: true });
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
